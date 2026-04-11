b19fa45916eb4cd35f1ca71e97039df41651f128
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-04-11 18:51:07 +0530

    Add files via upload

---
+++ Index/index.py
@@ -0,0 +1,16 @@
+# indexing = accessing elements of a sequencing using [] (indexing operator)
+# [ start : end : step ]
+
+credit_number = "1234-5678->9101-1121"
+
+# print(credit_number[0])
+# print(credit_number[:4])
+# print(credit_number[5:9])
+# print(credit_number[10:14])
+# print(credit_number[9:15])
+# print(credit_number[::8])
+# last_digits = credit_number[-4::]
+# last_digits = credit_number[]
+# print(f"XXXX-XXXX-XXXX-{last_digits}")
+
+print(credit_number[0:10:3])
